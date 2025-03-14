307fc5e1cc9d20f8ce33151710ecda73e727d0ac
Wajeepradit Prompan (AH) <wajeepradit.p@aapico.com>
2025-03-14 11:53:51 +0700

    add index.html

--- Watchguard_main.ps1
+++ Watchguard_main.ps1
@@ -7,7 +7,8 @@ $clientId =  "ec1e5f36-4262-4ead-a5d7-9ab8892a950b"
 $tenantId =  "a4722e58-ec99-4c3b-a34c-38620f1c4288"
 $graphScopes = "User.Read.All mail.read mail.send Mail.ReadWrite"
 # Authenticate the user
-Connect-MgGraph -ClientId $clientId -TenantId $tenantId -Scopes $graphScopes -UseDeviceAuthentication
+#Connect-MgGraph -ClientId $clientId -TenantId $tenantId -Scopes $graphScopes -UseDeviceAuthentication
+Connect-MgGraph
 $context = Get-MgContext
 $user = Get-MgUser -UserId $context.Account -Select 'displayName, id, mail, userPrincipalName'
 $emailAddress = "WGThreatAlert@pandasecurity.com"
@@ -24,3 +25,4 @@ $filteredMessages = $messages | Where-Object { $_.subject -match "block" }
 # ส่งออกผลลัพธ์เป็นไฟล์ CSV
 #$result | Export-Csv -Path "D:\Reportmail\WathguardBlockItem.csv" -NoTypeInformation
 #
+Set-Clipboard -Value $messages[0].Body.Content
